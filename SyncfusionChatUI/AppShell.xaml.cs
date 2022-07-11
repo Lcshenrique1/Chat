c154e97f899c099277ad6a97d51f5940c5be678d
Lcshenrique1 <lcshenrique@live.com>
2022-07-11 14:35:19 -0300

    first commit

---
+++ SyncfusionChatUI/AppShell.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using SyncfusionChatUI.ViewModels;
+using SyncfusionChatUI.Views;
+using Xamarin.Forms;
+
+namespace SyncfusionChatUI
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Shell.SetNavBarIsVisible(this, false);
+        }
+
+    }
+}
---
+++ SyncfusionChatUI/Config.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SyncfusionChatUI
+{
+    public static class Config
+    {
+        public static string WebChatBot
+        {
+            get => "add azure web chat key";
+
+        }
+        public static string DirectLineBot
+        {
+            get => "add aure direct line key";
+
+        }
+
+}
+}
